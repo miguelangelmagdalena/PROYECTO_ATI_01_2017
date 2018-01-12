cd10caf2ca2680079cf95e6479724275c62539bd
Miguel Magdalena <miguelangelmagdalena@gmail.com>
2018-01-12 06:42:55 -0400

    Agregando mas publicaciones al blog

--- RedparaATI/RedparaATI_modulo_estado/static/js/control_modulo_estado.js
+++ RedparaATI/RedparaATI_modulo_estado/static/js/control_modulo_estado.js
@@ -2,7 +2,7 @@ $(document).ready(function(){//Cuando cargan todos los elementos del DOM
 
 	//1. Mostramos la información dinamicamente desde el json
 	var i = 0;			//Valor Inicial
-	var max_value = 20; //Cantidad de elementos maxima en el json 
+	var max_value = 4; //Cantidad de elementos maxima en el json 
 	var increment = 2;	//Cantidad de elementos a mostrar hasta llegar al maximo
 	more_elements_callback(i,increment,max_value);
 
@@ -74,10 +74,146 @@ function more_elements_callback(index,increment,max_value){ //Funcion para agreg
 
 		for (i = index; i < max; i++) {
 
-			var publicacion = 			"<p class='data_title' title='data_title'>"+mydata[i].title+"</p>";
-			publicacion = publicacion + "<p class='data_description' title='data_description'>"+mydata[i].description+"</p>";
-			publicacion = publicacion + "<div class='data_image' title='data_image'><img class='data_image2' title='"+mydata[i].url+"' src='"+mydata[i].url+"'></img></div>";
+			/*var publicacion = 	"<p class='data_title' title='data_title'>"+mydata[i].title+"</p>";
+			publicacion += 		"<p class='data_description' title='data_description'>"+mydata[i].description+"</p>";
+			publicacion += 		"<div class='data_image' title='data_image'><img class='data_image2' title='"+mydata[i].url+"' src='"+mydata[i].url+"'></img></div>";
+			*/
+
+
+			
+			var publicacion = "	<div class='publicacion shadow border_radius'>";
+			publicacion +=			"<div class='publicacion2'>";
+			publicacion +=				"<div class='publicacion_barra_nombre'>";				
+			publicacion +=					"<a href=''>";
+			publicacion +=						"<img class='publicacion_foto_miniatura' src='"+mydata[i].foto_perfil+"'> ";
+			publicacion +=					"</a>";
+			publicacion +=					"<div class='publicacion_nombre '>";
+			publicacion +=						"<a href=''>"
+			publicacion +=							"<h2 class='publicacion_nombre2 titulo'>"+mydata[i].nombre_usuario+"</h2>";
+			publicacion +=						"</a>";					
+			publicacion +=					"</div>";
+			publicacion +=					"<button href='' class='publicacion_submenu_control_button'>";
+			publicacion +=						"<h2 class='publicacion_submenu_control titulo'>☰</h2>";
+			publicacion +=					"</button>";
+			publicacion +=				"</div>";
+			publicacion +=				"<div class='publicacion_barra_texto'>";
+			publicacion +=					"<p class='publicacion_texto'>";
+			publicacion +=						mydata[i].texto;
+			publicacion +=					"</p>";
+			publicacion +=					"<div class='publicacion_multimedia'>";
+			publicacion +=						"<a href=''>";
+			publicacion +=							"<img class='publicacion_multimedia2' src='"+mydata[i].multimedia_url+"'>";
+			publicacion +=						"</a>";
+			publicacion +=					"</div>";
+			publicacion +=				"</div>";
+			publicacion +=				"<!-- Barra de opciones de publicacion y comentario -->";
+			publicacion +=				"<div class='publicacion_barra_opciones'>";
+			publicacion +=					"<div class='publicacion_opciones flotar_derecha'>";
+			publicacion +=						"<button class='publicacion_opciones_button'>";
+			publicacion +=							"<img class='publicacion_icon' src='../static/images/icon-comentar.png'>" ;
+			
+			var comentarios_length = mydata[i].comentarios.length;
+			publicacion +=							"<span class='publicacion_opciones_texto comentario_font'>"+comentarios_length+"</span>";
+			publicacion +=						"</button>";
+			publicacion +=					"</div>";												
+			publicacion +=					"<div class='publicacion_opciones flotar_derecha'>";
+			publicacion +=						"<button class='publicacion_opciones_button'>";
+			publicacion +=							"<img class='publicacion_icon' src='../static/images/icon-nomegusta.png'>"; 
+			publicacion +=							"<span class='publicacion_opciones_texto comentario_font'>"+mydata[i].cant_dislike+" |</span>";
+			publicacion +=						"</button>";
+			publicacion +=					"</div>";
+			publicacion +=					"<div class='publicacion_opciones flotar_derecha'>";
+			publicacion +=						"<button class='publicacion_opciones_button'>";
+			publicacion +=							"<img class='publicacion_icon' src='../static/images/icon-megusta.png'> ";
+			publicacion +=							"<span class='publicacion_opciones_texto comentario_font'>"+mydata[i].cant_like+" |</span>";
+			publicacion +=						"</button>";
+			publicacion +=					"</div>";
+			publicacion +=					"<div class='publicacion_visto flotar_derecha'>";
+			publicacion +=							"<img class='publicacion_visto2' src='../static/images/icon-visto.png'> ";
+			publicacion +=							"<span class='publicacion_visto3 comentario_font'>"+mydata[i].cant_visualizaciones+" Visto</span>";
+			publicacion +=					"</div>";
+			publicacion +=					"<div class='publicacion_barra_opciones2'>";
+			publicacion +=						"<div class='fecha1 flotar_izquierda'>"+mydata[i].fecha_creacion+"</div>";
+			publicacion +=						"<div class='fecha2 flotar_izquierda'>(editado "+mydata[i].fecha_modificacion+")</div>";												
+			publicacion +=					"</div>";
+			publicacion +=				"</div>";
+
+			if(comentarios_length != 0){
+				publicacion +=				"<div class='seccion_comentarios'>";
+				for (j = 0; j < comentarios_length; j++) {
+					
+					var comentarios = "";
+					var aux1 = "";
+					var aux2 = "comentarios_foto_miniatura2";
+
+
+					if(mydata[i].comentarios[j].id_comentario_principal != -1){
+						aux1 = "comentarios_respuesta";
+						aux2 = "comentarios_foto_miniatura3";
+
+					} 
+					
+					comentarios +=				"<div class='seccion_comentarios2 "+aux1+"'>";
+					comentarios +=					"<div class='publicacion_barra_nombre'>";				
+					comentarios +=						"<a href=''>";
+					comentarios +=							"<img class='publicacion_foto_miniatura "+aux2+"' src='"+mydata[i].comentarios[j].foto_perfil+"'> ";
+					comentarios +=						"</a>";
+					comentarios +=						"<div class='publicacion_nombre '>";
+					comentarios +=							"<a href=''>";
+					comentarios +=								"<h2 class='publicacion_nombre2 titulo2'>"+mydata[i].comentarios[j].nombre_usuario+"</h2>";
+					comentarios +=							"</a>";
+					comentarios +=						"</div>";
+					comentarios +=					"</div>";
+					comentarios +=					"<div class='publicacion_barra_texto margin3'>";
+					comentarios +=						"<p class='publicacion_texto'>";
+					comentarios +=							mydata[i].comentarios[j].texto;
+					comentarios +=						"</p>";
+					comentarios +=					"</div>";
+					comentarios +=					"<!-- Barra de opciones de publicacion y comentario -->";
+					comentarios +=					"<div class='publicacion_barra_opciones'>";
+					comentarios +=						"<div class='publicacion_opciones flotar_derecha'>";
+					comentarios +=							"<button class='publicacion_opciones_button'>";
+					comentarios +=								"<img class='publicacion_icon' src='../static/images/icon-comentar.png'>" ;
+					comentarios +=								"<span class='publicacion_opciones_texto comentario_font'>"+mydata[i].comentarios[j].respuestas+"</span>";
+					comentarios +=							"</button>";
+					comentarios +=						"</div>	";
+					comentarios +=						"<div class='publicacion_opciones flotar_derecha'>";
+					comentarios +=							"<button class='publicacion_opciones_button'>";
+					comentarios +=								"<img class='publicacion_icon' src='../static/images/icon-nomegusta.png'>" ;
+					comentarios +=								"<span class='publicacion_opciones_texto comentario_font'>"+mydata[i].comentarios[j].cant_dislike+" |</span>";
+					comentarios +=							"</button>";
+					comentarios +=						"</div>";
+					comentarios +=						"<div class='publicacion_opciones flotar_derecha'>";
+					comentarios +=							"<button class='publicacion_opciones_button'>";
+					comentarios +=								"<img class='publicacion_icon' src='../static/images/icon-megusta.png'>" ;
+					comentarios +=								"<span class='publicacion_opciones_texto comentario_font'>"+mydata[i].comentarios[j].cant_like+" |</span>";
+					comentarios +=							"</button>";
+					comentarios +=						"</div>";
+					comentarios +=						"<div class='publicacion_barra_opciones2'>";
+					comentarios +=							"<div class='fecha1 flotar_izquierda'>"+mydata[i].comentarios[j].fecha_creacion+"</div>";											
+					comentarios +=						"</div>";
+					comentarios +=					"</div>";
+					comentarios +=				"</div>";			
+					publicacion += comentarios;
+				}
+				publicacion +=				"</div>";
+			}
+
+			publicacion +=			"</div>";
+			publicacion +=		"</div>";
+
 			$(publicacion).appendTo("#content");
+
+
+
+			//$("<div>Hola </div>").appendTo("#comentario");
+
+
+			//publicacion += 				"<div id='comentario'></div>";
+			//$("#comentario").html("<div>Hola </div>");
+			//$("#comentario").attr("id","");
+
+
 
 
 			/*
